a02ef9ffa47a36a1f58b576e3755986c47034f40
Kevin Liozon <kevinliozonpro@gmail.com>
2020-08-01 18:56:55 +0200

    first commit with router and some test pages

---
+++ global.js
@@ -0,0 +1,22 @@
+/*** PAGES ***/
+const pages = [{
+    name: 'Home',
+    href: '/'
+}, {
+    name: 'Projects',
+    href: '#/projects'
+}, {
+    name: 'View 1',
+    href: '#/view1'
+}, {
+    name: 'View 2',
+    href: '#/view2'
+}];
+
+
+
+
+/*** CONTAINERS ***/
+const wrapTemplate = document.getElementById('wrap');
+const wrapHeaderNav = document.getElementById('header-nav');
+const wrapFooterNav = document.getElementById('footer-nav');
---
+++ nav-generator.service.js
@@ -0,0 +1,3 @@
+for (let page of pages) {
+    wrapHeaderNav.innerHTML += '<a href="'+page.href+'">'+page.name+'</a>';
+}
