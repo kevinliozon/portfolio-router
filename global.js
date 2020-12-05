fc6b381e3f52e928a6a5f51cae8abc9d3824d57d
Kevin Liozon <kevinliozonpro@gmail.com>
2020-12-05 14:15:05 +0000

    Code optimisation. Addition of loader, error handler and event listerner when url is manually changed
    Optimisation: Moved history and page title update in get template function

--- global.js
+++ global.js
@@ -2,22 +2,22 @@
 const pages = [{
     name: 'Home',
     label: 'Navigate to home page',
-    href: '?page=home',
+    href: '#page=home',
     templatePath: '/home'
 }, {
     name: 'About & ethics',
     label: 'Learn more about my education, previous roles and ethics',
-    href: '?page=about',
+    href: '#page=about',
     templatePath: '/about'
 }, {
     name: 'Case studies',
     label: 'See some case studies from previous projects',
-    href: '?page=projects',
+    href: '#page=projects',
     templatePath: '/projects'
 }, {
     name: 'Driving change',
     label: 'Read about a change management case study',
-    href: '?page=change',
+    href: '#page=change',
     templatePath: '/change'
 }];
 
--- router.js
+++ router.js
@@ -1,35 +1,51 @@
 /**
  * Populate the wrapTemplate element with the targeted HTML template
  * 
- * @param {String} activeTemplate Path to the template
+ * @param {String} activeTemplate The relative path to the template
+ * @param {String} activePage Name of the page
+ * @param {String} activeUrl The relative url
  */
-function getTemplate (activeTemplate) {
+function getTemplate (activeTemplate, activePage, activeUrl) {
+    wrapTemplate.innerHTML = 'loading...';
+
     fetch(activeTemplate, { method: 'GET' })
-        .then( response => response.text() )
+        .then( response => response.text() ) // turn HTML response into a string
         .then( (content) => {
-                return wrapTemplate.innerHTML =  content
+                //This is where we update the address bar with the 'url' parameter
+                window.history.pushState({template: activeTemplate, page: activePage, url: activeUrl}, activePage, activeUrl);
+                document.title = activePage; // defines tab title
+                return wrapTemplate.innerHTML = content;
         })
-        .catch( error => console.error('error:', error) );
+        .catch( (error) => {
+            console.error('error:', error);
+            return wrapTemplate.innerHTML = 'This page does not exist';
+        });
 }
 
 window.addEventListener('load', (eventLoad) => {
 
-    (history && history.state) ? getTemplate(history.state.template) : getTemplate('/home'); // If no history, fallback onto home page
+    (history && history.state) ? 
+        getTemplate(history.state.template, history.state.page, history.state.url) : 
+        getTemplate('/home', 'Home', '#page=home'); // If no history, fall back onto home page
 
     Array.from(document.getElementsByClassName('nav__link')).forEach((navLink) => {
         navLink.addEventListener('click', function (e) {
-            relativePath = this.dataset.template;
-
-            getTemplate(relativePath);
-        
-            //This is where we update the address bar with the 'url' parameter
-            window.history.pushState({page: navLink.text, template: relativePath}, navLink.text, this.href);
-            document.title = navLink.text; // defines tab title
+            getTemplate(this.dataset.template, navLink.text, this.href);
         
             //This stops the browser from actually following the link
             e.preventDefault();
             e.stopPropagation();
         
         }, false)
-    })
+    });
 });
+
+window.addEventListener('hashchange', function(){
+    for (let page of pages) {
+        if (page.href === location.hash) { // does this url corresponds to any of those in the pages array?
+            getTemplate(page.templatePath, page.name, page.href);
+            return;
+        }
+    }
+    getTemplate('/home', 'Home', '#page=home'); // if not, fall back onto home page
+})
