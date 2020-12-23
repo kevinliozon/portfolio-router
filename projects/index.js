4c4c8993c3aac49f74a5567a464132dab5d1c6c2
Kevin Liozon <kevinliozonpro@gmail.com>
2020-12-23 12:43:03 +0000

    Converted view renderer script into module. Adapted the example project controller script

--- projects/index.js
+++ projects/index.js
@@ -1,2 +1,13 @@
-getViewTemplate('/projects/project1', document.getElementById('project1'));
-getViewTemplate('/projects/project2', document.getElementById('project2'));
+/**
+ * CONTROLLER FOR:
+ * PROJECTS PAGE
+ * 
+ * This script will replace the previous page's controller thanks to _getPageController()
+ * in router module
+ */
+
+/* On load or refresh */
+window.addEventListener('load', (event) => {
+    moduleViewRenderer.getViewTemplate('/projects/project1', document.getElementById('project1'));
+    moduleViewRenderer.getViewTemplate('/projects/project2', document.getElementById('project2'));
+});
