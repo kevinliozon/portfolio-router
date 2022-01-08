c9eee2585c13f95555edc32157c858d2465ab582
Kevin Liozon <kevinliozonpro@gmail.com>
2022-01-08 19:57:14 +0000

    Carousel v1

--- pages/home/index.js
+++ pages/home/index.js
@@ -54,4 +54,47 @@
 }, err => console.error('error:', err))
 .finally(() => {
   moduleRouter.linksListener('js-link--content');
+
+
+
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function showSlides(n) {
+    console.log('link')
+    // Array with all navigation links
+    Array.from(document.getElementsByClassName('column')).forEach((link) => {
+        console.log(link)
+      // Event listener on each link
+      link.addEventListener('click', function(e) {
+        console.log(link, link.dataset.slide)
+        showSlides(slideIndex = link.dataset.slide);
+        //This prevents the browser from actually following the default link
+        e.stopPropagation();
+        e.preventDefault();
+      }, false)
+    })
+
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 })
