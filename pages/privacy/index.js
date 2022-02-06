4e0c638e67e05c844a14d31df7ef7f1c250876cd
Kevin Liozon <kevinliozonpro@gmail.com>
2022-02-06 15:10:35 +0000

    Sidenav for Privacy page

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -5,20 +5,17 @@
  * This script will replace the previous page's controller thanks to getPageController method in router module
  */
 
-/**
- * PROMISE - 
- * Final state: Building the navigation listener
- * 
- */
  new Promise((resolve, reject) => {
-  if (projects) {
-    resolve(projects); // We pass all the elements used for controls
+  if (document.getElementById('sidenav')) {
+    resolve(); // Detect the links and build their navigation listener
   } else {
     // Will listen to the links across the page in all cases
-    reject('Pojects do not exist')
+    reject('Sidenav element does not exist')
   }
 })
-.then(result => {}, err => console.error('error:', err))
+.then(result => {
+  moduleRouter.hashListener('js-link--hash');
+}, err => console.error('error:', err))
 .finally(() => {
   moduleRouter.linksListener('js-link');
 })
